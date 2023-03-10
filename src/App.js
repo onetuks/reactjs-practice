ab2e6a7662ae32a2361d2356e7b31f2b9f0f6d69
SY97P <onetuks@naver.com>
2023-03-10 14:51:23 +0900

    styled-components 사용법 - 기본

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import styled from 'styled-components';
 
+const Father = styled.div`
+  display: flex;
+`;
+
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
+
+const Text = styled.span`
+  color: white;
+  font-size: 40px;
+`;
+
 function App() {
   return (
-    <div style={{display:"flex"}}>
-      <div style={{ backgroundColor:"teal", width:100, height:100}}></div>
-      <div style={{ backgroundColor:"tomato", width:100, height:100}}></div>
-    </div>
+    <Father>
+      <Box bgColor="teal">
+        <Text>Hello</Text>
+      </Box>
+      <Box bgColor="tomato" />
+      <Circle bgColor="cyan"/>
+    </Father>
   );
 }
 
