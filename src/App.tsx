d9eaa25b75772e6020e1fe403fb9461c96dc6340
SY97P <onetuks@naver.com>
2023-03-15 18:26:41 +0900

    typescript init

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Container = styled.div``;
+
 function App() {
   return (
-    <div>
+    <Container>
       Hello
-    </div>
+    </Container>
   );
 }
 
