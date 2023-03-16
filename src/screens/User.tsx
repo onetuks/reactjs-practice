f66cb0d0a35618d58ac4a54061eed585222bb5e3
SY97P <onetuks@naver.com>
2023-03-16 21:22:57 +0900

    react-router-dom@6.x - Outlet 주석 추가

--- src/screens/User.tsx
+++ src/screens/User.tsx
@@ -7,7 +7,6 @@ import { users } from "../db";
  * - 현재 컴포넌트에 Router.tsx에 선언한 children이 존재하는 경우 자식 컴포넌트를 렌더링하기 위한 컴포넌트
  * - createBrowserRouter() 메소드(@6.x 버전) 사용 시 활용
 */
-
 function User() {
     const { userId } = useParams();
     const username = users[Number(userId)-1].name;
@@ -15,6 +14,13 @@ function User() {
         <h1>User</h1>
         <p>UserId: {userId} has name of {username}.</p>
         <hr/>
+        {/* 
+            ** to="/followers" 로 하면 안 되는 이유 **
+
+            localhost:3000/user/1/followers 가 아니라
+            localhost:3000/follower로 링크됨. 
+            '/' 는 root를 의미하기 때문.
+         */}
         <Link to="followers">See {username}'s followers.</Link>
         <Outlet />
     </div>;
